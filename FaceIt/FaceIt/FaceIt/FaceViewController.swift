02ffd480eecadd801161695f60e1b574a193fce2
Michael C <mikecu88@gmail.com>
2017-01-17 16:35:29 +1300

    FaceIt project started

---
+++ FaceIt/FaceIt/FaceIt/FaceViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by Mojo on 17/01/17.
+//  Copyright © 2017 NoOrg. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+
+
+}
+
